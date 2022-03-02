d4bb1b309271b7f7fb89690f4e80b471420a559a
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-03-02 10:57:31 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_Collection_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_Collection_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> payClients = new Dictionary<string, int>();
+
+            payClients.Add("Вася", 100);
+            payClients.Add("Маша", 20);
+            payClients.Add("Петр", 9);
+
+            int check = 0;
+
+            foreach (var item in payClients)
+            {
+                check = item.Value + check;
+                Console.WriteLine("Заплатил клиент - " + item.Key + "\nУ нас на счету - " + check);
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
